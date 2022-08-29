df57de377c6d6df742f5e82615a3fee1faf54134
Chris Ding <chrisding7@berkeley.edu>
2022-08-29 10:33:07 -0700

    commit tree ccecdcf2d54abd62d68ee485ce3517ef4467c489

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+function saturdayFun(arg="roller-skate") {
+    return `This Saturday, I want to ${arg}!`;
+}
+
+function mondayWork(arg="go to the office") {
+    return `This Monday, I will ${arg}.`;
+}
+
+function wrapAdjective(flair="*") {
+    return function(adjective="special") {
+        return `You are ${flair}${adjective}${flair}!`;
+    }
+}
